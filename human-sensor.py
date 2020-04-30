abab8f705af4975e35614940c0a1899d88435343
p0x0q <p0x0q_jp@gmail.com>
2020-05-01 01:46:20 +0900

    support --blind-spot

--- human-sensor.py
+++ human-sensor.py
@@ -36,26 +36,46 @@ parser.add_argument(
     "--sleep",
     required=False,
     default=0.2,
-    help="sleep time(example: 1 = 1 second)",
+    help="映像の差分比較を行う際の間隔(second)",
+)
+
+parser.add_argument(
+    "--blind-spot",
+    required=False,
+    help="長方形でカメラの映像を上塗りし、物体検出の対象外部分を描画します。 (書式：rect_start_x,rect_start_y,rect_end_x,rect_end_y, example: 0,0,150,150)",
 )
 
 args = parser.parse_args()
 #camera_id='2', diff_threshold='128'
 
+def check_frame(frame):
+    if args.blind_spot:
+        rect_str = str(args.blind_spot).split(',')
+        rects = []
+        for val in rect_str:
+            rects.append(int(val))
+        rect = list(rects)
+        if len(rect) != 4:
+            print("--blind-spot は長さが4つではありません: length({})".format(len(rect)))
+            return frame
+        cv2.rectangle(frame, rect, (0,255,0), cv2.FILLED)
+    return frame
+
 capture = cv2.VideoCapture(int(args.camera_id))
 if args.show:
     ret, frame = capture.read()
+    frame = check_frame(frame)
     cv2.imshow("Waiting for this window to close",frame)
     cv2.waitKey(0)
-
 
 ret, frame = capture.read()
 
 while(True):
     im_before = frame
-    time.sleep(args.sleep)
+    time.sleep(float(args.sleep))
     print("")
     ret, frame = capture.read()
+    frame = check_frame(frame)
     im_after = frame
     im_diff = im_before.astype(int) - im_after.astype(int)
     diff = im_diff.max()
